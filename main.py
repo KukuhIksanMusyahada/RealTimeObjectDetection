3b6ad325c8c46145e71b06d290c2f576d11432d5
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2023-01-09 23:13:49 +0700

    labelling done

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import uuid
 import time
 import os
 
+from git.repo.base import Repo
+
 # Set things up
 
 np.random.seed(13618045)
@@ -17,15 +19,15 @@ captured_images = os.path.join(images_path,'captured_images')
 
 # Download and process the data
 class CreateTrainingImages:
-    def __init__(self,path=captured_images):
+    def __init__(self,path=images_path):
         self.path = path
-        self.init_labels = ['kukuh','bottle']
+        self.init_labels = []
         self.num_imgs = 20
         self.add_label = True
 
     def addLabel(self):
         while self.add_label:
-            add_label = input("add more labels?\n \t1. Yes\n \t 2. No \n")
+            add_label = input("add more labels?\n \t1. Yes\n \t2. No \n")
             if add_label == '1':
                 new_label = input("Type new label: \n")
                 self.init_labels.append(new_label)
@@ -33,8 +35,11 @@ class CreateTrainingImages:
                 self.add_label = False
     
     def createDir(self):
+        captured_images_path = os.path.join(self.path,'captured_imgs')
+        if not os.path.exists(captured_images_path):
+            os.mkdir(captured_images_path)
         for label in self.init_labels:
-            lab_path = os.path.join(self.path, label)
+            lab_path = os.path.join(captured_images_path, label)
             if not os.path.exists(lab_path):
                 os.mkdir(lab_path)
 
@@ -46,7 +51,7 @@ class CreateTrainingImages:
             for num_img in range(self.num_imgs):
                 print(f"Images collected= {num_img}")
                 _,img = cap.read()
-                img_name = os.path.join(self.path,label,f'label.{str(uuid.uuid1())}.jpg')
+                img_name = os.path.join(self.path,label,f'{label}.{str(uuid.uuid1())}.jpg')
                 cv2.imwrite(img_name, img)
                 cv2.imshow('image',img)
                 time.sleep(2)
@@ -55,12 +60,22 @@ class CreateTrainingImages:
                     break
             cap.release()
             cv2.destroyAllWindows()
+    def labelling_imgs(self):
+        label_path = os.path.join(self.path,'label_imgs')
+        if not os.path.exists(label_path):
+            Repo.clone_from('https://github.com/tzutalin/labelImg', label_path)
+        os.chdir(label_path)
+        os.system('pyrcc5 -o libs/resources.py resources.qrc')
+        os.system('python labelImg.py')
+
+
 
 
     def createData(self):
         self.addLabel()
         self.createDir()
-        self.captureImgs()
+        # self.captureImgs()
+        self.labelling_imgs()
         
         
     
