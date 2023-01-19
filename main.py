1a9f8f97a2c6804fe1828fcd3f232d24d57596b5
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2023-01-19 11:36:31 +0700

    success importing pretrained model efficient det

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import cv2
 import uuid
 import time
 import os
-import traceback
+import urllib.request
+import tarfile
 import shutil
 import random
 import re
+import object_detection
 
 from git.repo.base import Repo
 
@@ -16,20 +18,25 @@ from git.repo.base import Repo
 np.random.seed(13618045)
 min_confidence = 0.3
 work_space_path = os.path.dirname( os.path.abspath(__file__) )
+parent_path = os.path.join(work_space_path,os.pardir)
+PROTOC_PATH = 'C://Program Files//Google Protobuff//'
+
 # images_path = os.path.join(work_space_path ,'images')
 # captured_images = os.path.join(images_path,'captured_images')
 # colors = np.random.uniform(0,255,size= (len(labels)),3)
 
 CUSTOM_MODEL_NAME = 'efficient_det_models' 
-PRETRAINED_MODEL_NAME = 'efficientdet_d0_coco17_tpu-32'
+PRETRAINED_MODEL_NAME = 'efficientdet_d0_coco17_tpu-32.tar.gz'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz'
 TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
 LABEL_MAP_NAME = 'label_map.pbtxt'
 
 paths = {
     'WORK_SPACE_PATH' : os.path.dirname( os.path.abspath(__file__) ),
+    'PARENT_PATH' : os.path.join(os.getcwd(),os.pardir),
+    'TF_PATH'   :   os.path.join(parent_path,'TensorFlow'),
     'SCRIPTS_PATH': os.path.join(work_space_path,'TFscripts'),
-    'APIMODEL_PATH': os.path.join(work_space_path,'models'),
+    'APIMODEL_PATH': os.path.join(parent_path,'TensorFlow','models'),
     'ANNOTATION_PATH': os.path.join(work_space_path,'annotations'),
     'IMAGE_PATH': os.path.join(work_space_path,'images'),
     'CAPTURED_IMAGES_PATH' : os.path.join(work_space_path,'images','captured_imgs'),
@@ -40,7 +47,7 @@ paths = {
     'OUTPUT_PATH': os.path.join(work_space_path,'models',CUSTOM_MODEL_NAME, 'export'), 
     'TFJS_PATH':os.path.join(work_space_path,'models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
     'TFLITE_PATH':os.path.join(work_space_path,'models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join(work_space_path,'protoc')
+    'PROTOC_PATH':'C://Program Files//Google Protobuff'
  }
 
 
@@ -56,7 +63,7 @@ files = {
 class CreateTrainingImages:
     def __init__(self,path=paths):
         self.path = path
-        self.init_labels = []
+        self.init_labels = ['Kukuh','Botol']
         self.num_imgs = 5
         self.add_label = True
         self.train_ratio = 0.8
@@ -174,9 +181,30 @@ class GetAndTrainModels:
     def __init__(self, paths = paths, files= files):
         self.paths = paths
         self.files = files
+        self.training_data = CreateTrainingImages()
+        self.labels = self.training_data.init_labels
 
-    def create_path(self):
-        pass
+    def download_all_needed_data(self):
+        # download pretrained model
+        if not os.path.exists(self.paths['MODEL_PATH']):
+            os.mkdir(self.paths['MODEL_PATH'])
+        if not os.path.exists(self.paths['PRETRAINED_MODEL_PATH']):
+            os.mkdir(self.paths['PRETRAINED_MODEL_PATH'])
+        if not os.path.exists(os.path.join(self.paths['PRETRAINED_MODEL_PATH'],PRETRAINED_MODEL_NAME[:-7])):
+            urllib.request.urlretrieve(PRETRAINED_MODEL_URL,PRETRAINED_MODEL_NAME)
+            tar_ref = tarfile.open(PRETRAINED_MODEL_NAME,'r')
+            tar_ref.extractall(self.paths['PRETRAINED_MODEL_PATH'])
+            tar_ref.close()
+       
+    def create_record(self):
+        if not os.path.exists(os.path.join(work_space_path,'annotations')):
+            os.mkdir(os.path.join(work_space_path,'annotations'))
+        with open(self.files['LABELMAP'], 'w') as f:
+            for id,label in enumerate(self.labels):
+                f.write('item { \n')
+                f.write(f'\tname:\'{label}\'\n')
+                f.write(f'\tid:{id+1}\n')
+                f.write('}\n')
         
 
     
@@ -185,6 +213,23 @@ class GetAndTrainModels:
 
 
 
+#  if not os.path.exists(os.path.join(self.paths['APIMODEL_PATH'], 'research', 'object_detection')):
+#             Repo.clone_from("https://github.com/tensorflow/models",self.paths['APIMODEL_PATH'])
+#         #download protocol buffer
+#         PROTOC_URL = "https://github.com/protocolbuffers/protobuf/releases/download/v21.12/protoc-21.12-win64.zip"
+#         # if not os.path.exists(os.path.join(self.paths['PROTOC_PATH'],'protoc-21.12-win64.zip')):
+#         #     urllib.request.urlretrieve(PROTOC_URL, 'protoc-21.12-win64.zip')
+#             # shutil.move('protoc-21.12-win64.zip',self.paths['PROTOC_PATH'])
+#         #extract protocol buffer 
+#         # zip_file = os.path.join(self.paths['PROTOC_PATH'],'protoc-21.12-win64.zip')
+#         # zip_ref = zipfile.ZipFile(zip_file, 'r')
+#         # zip_ref.extractall(self.paths['PROTOC_PATH'])
+#         # Set Up Protocol buffer
+#         os.environ['PATH'] += os.path.abspath(os.path.join(self.paths['PROTOC_PATH'], 'bin'))
+#         os.chdir(os.path.join(self.paths['APIMODEL_PATH'],'research'))
+#         os.system('protoc object_detection/protos/*.proto --python_out=.')
+#         os.system('copy object_detection\\packages\\tf2\\setup.py setup.py')
+#         os.system('python -m pip install --use-feature=2020-resolver')
 
 
 
@@ -197,6 +242,8 @@ class GetAndTrainModels:
 
 # Run
 if __name__=="__main__":
-    data = CreateTrainingImages()
-    data.createData()
-    print('test')
+    # data = CreateTrainingImages()
+    # data.createData()
+    model = GetAndTrainModels()
+    model.download_all_needed_data()
+    model.create_record()
